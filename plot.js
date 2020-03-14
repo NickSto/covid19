29f2c36cf6271b06389b807a997d56995b656e00
Nick <nmapsy@gmail.com>
2020-03-14 18:29:00 -0400

    Initial Javascript version using Plot.ly.

---
+++ plot.js
@@ -0,0 +1,171 @@
+
+let data = [];
+const DATA_URL_BASE = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports';
+
+function covidMain() {
+  for (let dayEntry of getDates()) {
+    dayEntry.status = 'loading';
+    data.push(dayEntry);
+  }
+
+  for (let dayEntry of data) {
+    makeRequest(
+      `${DATA_URL_BASE}/${dayEntry.name}.csv`,
+      function() {appendData(this, dayEntry);}
+    );
+  }
+}
+
+function appendData(xhr, dayEntry) {
+  if (xhr.status == 200) {
+    let dailyDataRaw = CSV.parse(xhr.responseText);
+    dayEntry.data = processData(dailyDataRaw);
+    dayEntry.status = 'loaded';
+  } else {
+    dayEntry.status = 'failed';
+  }
+  if (isDoneLoading(data)) {
+    useData(data);
+  }
+}
+
+function isDoneLoading(data) {
+  for (let dayEntry of data) {
+    if (dayEntry.status === 'loading') {
+      return false;
+    }
+  }
+  return true;
+}
+
+function useData(data) {
+
+  let plotData = [];
+
+  for (let country of ['Italy', 'South Korea']) {
+    let [dates, counts] = getPlaceSeries(data, country);
+    plotData.push({name:country, x:dates, y:counts});
+  }
+
+  let waitElem = document.getElementById('waiting');
+  let loadElem = document.getElementById('loaded');
+  waitElem.style.display = 'none';
+  loadElem.style.display = null;
+
+  let plotContainer = document.getElementById('plot-container');
+
+  Plotly.newPlot(plotContainer, plotData, {margin: {t:0}});
+}
+
+function getPlaceSeries(data, country) {
+  let dates = [];
+  let counts = [];
+  for (let dayEntry of data) {
+    if (dayEntry.status !== 'loaded') {
+      continue
+    }
+    let total = null;
+    for (let row of dayEntry.data) {
+      if (row.country === country && row.confirmed !== null) {
+        if (total === null) {
+          total = 0;
+        }
+        total += row.confirmed;
+      }
+    }
+    if (total !== null) {
+      dates.push(dayEntry.date);
+      counts.push(total);
+    }
+  }
+  return [dates, counts];
+}
+
+function makeRequest(url, callback) {
+  let request = new XMLHttpRequest();
+  request.addEventListener('loadend', callback);
+  request.open('GET', url);
+  request.send();
+}
+
+function processData(rawTable) {
+  let table = [];
+  let rowNum = 0;
+  for (let row of rawTable) {
+    rowNum++;
+    let region = row[0];
+    let country = row[1];
+    let confirmed = row[3];
+    let deaths = row[4];
+    let recovered = row[5];
+    if (TRANSLATIONS[region]) {
+      region = TRANSLATIONS[region];
+    }
+    if (TRANSLATIONS[country]) {
+      country = TRANSLATIONS[country];
+    }
+    if (typeof confirmed === 'string') {
+      if (rowNum !== 1) {
+        console.error(`Invalid 'Confirmed' number on row ${rowNum}: ${confirmed} (updated ${row[2]})`);
+      }
+      continue;
+    }
+    table.push({
+      region: region, country: country, confirmed: confirmed, deaths: deaths, recovered: recovered
+    })
+  }
+  return table;
+}
+
+function getDates() {
+  let dates = [];
+  const StartYear = 2020;
+  const now = new Date();
+  const thisYear = now.getFullYear();
+  const thisMonth = now.getMonth()+1;
+  const thisDay = now.getDate();
+  let done = false;
+  for (let year = StartYear; year <= thisYear; year++) {
+    for (let month = 1; month <= 12; month++) {
+      for (let day = 1; day <= 31; day++) {
+        if (year == StartYear && month == 1 && day < 22) {
+          continue;
+        }
+        let dayStr = day.toString().padStart(2, '0');
+        let monthStr = month.toString().padStart(2, '0');
+        let dateStr = `${monthStr}-${dayStr}-${year}`;
+        let dateObj = new Date(year, month-1, day);
+        dates.push({name:dateStr, date:dateObj});
+        if (year == thisYear && month == thisMonth && day == thisDay) {
+          done = true;
+          break;
+        }
+      }
+      if (done) {
+        break;
+      }
+    }
+    if (done) {
+      break;
+    }
+  }
+  return dates;
+}
+
+const TRANSLATIONS = {
+  'District of Columbia': 'DC',
+  'Mainland China': 'China',
+  'Iran (Islamic Republic of)': 'Iran',
+  'Republic of Korea': 'South Korea',
+  'Korea, South': 'South Korea',
+  'Britain': 'UK',
+  'United Kingdom': 'UK',
+  'United States': 'US',
+  'Hong Kong SAR': 'Hong Kong',
+  'Taipei and environs': 'Taiwan',
+  'Taiwan*': 'Taiwan',
+  'occupied Palestinian territory': 'Palestine',
+  'Russian Federation': 'Russia',
+};
+
+window.addEventListener('load', covidMain, false);
